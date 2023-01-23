a5152a4ccfcd64ec27f391b1c38ccc7e0b066a82
Suchita Manohare <38423185+suchitadudhmal358@users.noreply.github.com>
2023-01-23 13:12:50 +0530

    Routing in ReactJS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Route,
+  Link,
+  Switch,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import Layout from "./Layout";
+import NotFound from "./NotFound";
+import User from "./User";
+import Visit from "./Visit";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Layout />}>
+          <Route path="/user" element={<User />} />
+          <Route path="/visit" element={<Visit />} />
+          <Route element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/Layout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/user">User</Link>
+        </li>
+        <li>
+          <Link to="/visit">Visit</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
